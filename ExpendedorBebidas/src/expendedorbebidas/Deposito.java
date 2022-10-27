afea4c0cc45b936b7891ca1a1b4af5e5c3190a8c
CataBravoH <cbravo2019@udec.cl>
2022-10-27 01:50:44 -0300

    Desarrollo clase Deposito

--- ExpendedorBebidas/src/expendedorbebidas/Deposito.java
+++ ExpendedorBebidas/src/expendedorbebidas/Deposito.java
@@ -1,5 +1,22 @@
 package expendedorbebidas;
 
+import java.util.ArrayList;
+
 public class Deposito {
+    private ArrayList <Bebida> bebida;
     
+    public Deposito(){
+        bebida = new ArrayList <Bebida>();
+    }
+    public void addBebida(Bebida b){
+        if(b != null){
+            bebida.add(b);
+        }
+    }
+    public Bebida getBebida(){
+        if(bebida.size() == 0)
+            return null;
+        else
+            return bebida.remove(0);
+    }
 }
