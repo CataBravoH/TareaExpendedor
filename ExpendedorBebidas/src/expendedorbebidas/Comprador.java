1e151cb00e30a8dc40fd9efd9902da1ca2d7e56d
CataBravoH <cbravo2019@udec.cl>
2022-10-27 02:11:38 -0300

    Desarrollando Comprador

--- ExpendedorBebidas/src/expendedorbebidas/Comprador.java
+++ ExpendedorBebidas/src/expendedorbebidas/Comprador.java
@@ -1,5 +1,27 @@
 package expendedorbebidas;
 
 public class Comprador {
+    private int vuelto;
+    private String tomo;
     
+    public Comprador(Moneda m, int cualBebida, Expendedor exp){
+        Bebida b= exp.comprarBebida(m, cualBebida);
+        tomo=null;
+        vuelto=0;
+        Moneda aux = null;
+        while((aux = exp.getVuelto()) != null){
+            vuelto = vuelto + aux.getValor();
+        }
+        if(b != null){
+            tomo = b.beber();
+        }
+    }
+    
+    public int cuantoVuelto(){
+        return vuelto;
+    }
+    
+    public String queBebiste(){
+        return tomo;
+    }
 }
