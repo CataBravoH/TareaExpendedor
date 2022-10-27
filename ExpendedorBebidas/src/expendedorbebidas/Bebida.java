255327c0179e0d1b2749c483198d8350ec00e6b6
CataBravoH <cbravo2019@udec.cl>
2022-10-27 01:37:38 -0300

    Desarrollo clase Bebida

--- ExpendedorBebidas/src/expendedorbebidas/Bebida.java
+++ ExpendedorBebidas/src/expendedorbebidas/Bebida.java
@@ -1,5 +1,35 @@
 package expendedorbebidas;
 
-public class Bebida {
+abstract public class Bebida {
+    private int numSerie;
     
+    public Bebida(int numSerie){
+        this.numSerie = numSerie;
+    }
+    public int getSerie(){
+        return this.numSerie;
+    }
+    public abstract String beber();
+}
+
+class Sprite extends Bebida{
+    
+    public Sprite(int numSerie){
+        super(numSerie);
+    }
+    @Override
+    public String beber(){
+        return new String ("Sprite");
+    }
+}
+
+class CocaCola extends Bebida{
+    
+    public CocaCola(int numSerie){
+        super(numSerie);
+    }
+    @Override
+    public String beber(){
+        return new String ("CocaCola");
+    }
 }
